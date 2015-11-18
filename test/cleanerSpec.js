99f3ff4e4890e9735dfb7aeba588945368eab673
Ivan Blazevic <ivanblazevic17@gmail.com>
2015-11-18 11:48:12 +0100

    tests for new functionality and gulp

--- test/cleanerSpec.js
+++ test/cleanerSpec.js
@@ -45,9 +45,16 @@ function checkEmptyTable(params) {
   };
 }
 
-// function databaseCleanerDeprecated(callback, results) {
-//   cleaner(results.connect[0], callback);
-// }
+function checkSequence(params) {
+  return function check(callback, results) {
+    var q = 'SELECT nextval(\'' + params.table + '_id_seq\');';
+    results.connect[0].query(q, callback);
+  }
+}
+
+function databaseCleanerDeprecated(callback, results) {
+  cleaner(results.connect[0], callback);
+}
 
 function databaseCleaner(options) {
   return function(callback, results) {
@@ -65,29 +72,29 @@ function connect(params) {
 
 describe('postgres', function() {
 
-  beforeEach(function(done) {
+  before(function(done) {
     async.auto({
       connectPostgres: connect({database: 'postgres'}),
-      dropDb:['connectPostgres', dropDatabase({database: 'cleaner'})]
+      dropDb: ['connectPostgres', dropDatabase({database: 'cleaner'})],
+      createDb: ['dropDb', createDatabase({database: 'cleaner'})]
     }, done);
   });
 
-  beforeEach(function(done) {
+  before(function(done) {
     async.auto({
-      connectPostgres: connect({database: 'postgres'}),
-      createDb:['connectPostgres', createDatabase({database: 'cleaner'})]
+      connect: connect({database: 'cleaner'}),
+      create1: ['connect', createTable({table: 'table1'})],
+      create2: ['connect', createTable({table: 'table2'})],
+      create3: ['connect', createTable({table: 'table3'})]
     }, done);
   });
 
   beforeEach(function(done) {
     async.auto({
       connect: connect({database: 'cleaner'}),
-      create1: ['connect', createTable({table: 'table1'})],
-      create2: ['connect', createTable({table: 'table2'})],
-      create3: ['connect', createTable({table: 'table3'})],
-      insert1: ['create1', insertValue({table: 'table1'})],
-      insert2: ['create2', insertValue({table: 'table2'})],
-      insert3: ['create3', insertValue({table: 'table3'})]
+      insert1: ['connect', insertValue({table: 'table1'})],
+      insert2: ['connect', insertValue({table: 'table2'})],
+      insert3: ['connect', insertValue({table: 'table3'})]
     }, done);
   });
 
@@ -97,12 +104,80 @@ describe('postgres', function() {
       clean: ['connect', databaseCleaner({type: 'delete'})],
       check1: ['clean', checkEmptyTable({table: 'table1'})],
       check2: ['clean', checkEmptyTable({table: 'table2'})],
-      check3: ['clean', checkEmptyTable({table: 'table3'})]
+      check3: ['clean', checkEmptyTable({table: 'table3'})],
+      checkSeq1: ['clean', checkSequence({table: 'table1'})],
+      checkSeq2: ['clean', checkSequence({table: 'table2'})],
+      checkSeq3: ['clean', checkSequence({table: 'table3'})]
     }, function(err, results) {
       if (err) throw(err);
       should(results.check1.rows.length).equal(0);
       results.check2.rows.length.should.equal(0);
       results.check3.rows.length.should.equal(0);
+      results.checkSeq1.rows.length.should.equal(1);
+      results.checkSeq2.rows.length.should.equal(1);
+      results.checkSeq3.rows.length.should.equal(1);
+      done();
+    });
+  });
+
+  it('should delete all tables', function(done) {
+    async.auto({
+      connect: connect({database: 'cleaner'}),
+      clean: ['connect', databaseCleaner({type: 'truncate'})],
+      check1: ['clean', checkEmptyTable({table: 'table1'})],
+      check2: ['clean', checkEmptyTable({table: 'table2'})],
+      check3: ['clean', checkEmptyTable({table: 'table3'})],
+      checkSeq1: ['clean', checkSequence({table: 'table1'})],
+      checkSeq2: ['clean', checkSequence({table: 'table2'})],
+      checkSeq3: ['clean', checkSequence({table: 'table3'})]
+    }, function(err, results) {
+      if (err) throw(err);
+      should(results.check1.rows.length).equal(0);
+      results.check2.rows.length.should.equal(0);
+      results.check3.rows.length.should.equal(0);
+      results.checkSeq1.rows.length.should.equal(1);
+      results.checkSeq2.rows.length.should.equal(1);
+      results.checkSeq3.rows.length.should.equal(1);
+      done();
+    });
+  });
+
+  it('should delete all tables deprecated', function(done) {
+    async.auto({
+      connect: connect({database: 'cleaner'}),
+      clean: ['connect', databaseCleanerDeprecated],
+      check1: ['clean', checkEmptyTable({table: 'table1'})],
+      check2: ['clean', checkEmptyTable({table: 'table2'})],
+      check3: ['clean', checkEmptyTable({table: 'table3'})],
+      checkSeq1: ['clean', checkSequence({table: 'table1'})],
+      checkSeq2: ['clean', checkSequence({table: 'table2'})],
+      checkSeq3: ['clean', checkSequence({table: 'table3'})]
+    }, function(err, results) {
+      if (err) throw(err);
+      should(results.check1.rows.length).equal(0);
+      results.check2.rows.length.should.equal(0);
+      results.check3.rows.length.should.equal(0);
+      results.checkSeq1.rows.length.should.equal(1);
+      results.checkSeq2.rows.length.should.equal(1);
+      results.checkSeq3.rows.length.should.equal(1);
+      done();
+    });
+  });
+
+  it('should error cause of wrong type and options', function(done) {
+    async.auto({
+      connect: connect({database: 'cleaner'})
+    }, function(err, results) {
+      try {
+        cleaner({type: 'wrong'}, results.connect[0], function() {});
+      } catch (e) {
+        e.message.should.eql('Unrecognized type: wrong');
+      }
+      try {
+        cleaner({wrong: 'delete'}, results.connect[0], function() {});
+      } catch (e) {
+        e.message.should.eql('"type" option is required');
+      }
       done();
     });
   });
